6a573dd89f48c06ca082618167c3cbbf64535983
unknown <bhorsun@yahoo.com>
2019-02-15 23:15:01 -0800

    building the meal Services and model

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default{
+    meals: [
+        {
+            id: 1,
+            name: 'Fried Rice and Stake',
+            size: 'medium',
+            price: '450'
+        },
+        {
+            id: 2,
+            name: 'Jollof Rice',
+            size: 'large',
+            price: '550'
+        },
+        {
+            id: 3,
+            name: 'Beans',
+            size: 'small',
+            price: '150'
+        },
+        {
+            id: 4,
+            name: 'Yam and Egg',
+            size: 'large',
+            price: '300'
+        }
+    ]
+}
