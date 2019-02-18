a1f8fb1a45b8449e6cc9652409b401f9de474b74
unknown <bhorsun@yahoo.com>
2019-02-18 06:53:11 -0800

    completion of order routes on Api

---
+++ api/models/order.model.js
@@ -0,0 +1,11 @@
+class Order  {
+    constructor() {
+        this.id = null,
+        this.address = null,
+        this.food = null,
+        this.quantity = null,
+        this.price = null
+    }
+}
+
+export default Order;
